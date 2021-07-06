697001b427cec05fa7cf781e99a5dc569b413139
Lluis Casamajor <lluischess@gmail.com>
2021-07-06 16:17:44 +0200

    tip 1.1

--- Tips.php
+++ Tips.php
@@ -1,6 +1,8 @@
 <?php
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# utilizar ? en caso contrario en isset y empty
+# utilizar ? en caso contrario 
+#isset = es nulo?
+#empty = esta vacio?
 # Ejemplo 1  despues del ? se pone la otra condicion
 $action = isset($_POST['action']) ? $_POST['action'] : '';
 
