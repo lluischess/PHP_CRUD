fd9fb6c655f6d718f9229f2d6ad7cd8e393a3691
Lluis Casamajor <lluischess@gmail.com>
2021-07-17 00:16:40 +0200

    Tips php

---
+++ Entorno.php
@@ -0,0 +1,75 @@
+<?php
+
+#------------Tener un entorno PHP en local:
+
+# 1) Herramienta numero 1: 
+
+    # EDITORES DE CODIGO PARA PHP 2021:
+    # - Visual Studio Code
+
+    # IDE PARA PHP 2021:
+    # - Apache NetBeans
+
+
+# 2) Herramienta numero 2: 
+
+    # - Servidor de aplicaciones WEB
+    # 1- WAMP SERVER
+    # 2- XAMPP SERVER
+    
+    
+# 3) Herramienta numero 3: 
+
+    # Control de versiones Git Bash;
+    # Git for windows
+
+
+# 4) Herramienta numero 4: 
+
+    # Declarar variables de entorno:
+    # Windows %%PATH%%
+
+# 5) Herramienta numero 5:
+
+    # Habilitar herramientas APACHE:
+    # - rewrite_module
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
