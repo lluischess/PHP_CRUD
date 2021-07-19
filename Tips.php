306db8576c2b59e1f0f9aef6406fa661940a14bf
Lluis Casamajor <lluischess@gmail.com>
2021-07-19 19:13:54 +0200

    functions

--- Tips.php
+++ Tips.php
@@ -5,9 +5,9 @@
 # 2) Echo rapido en php
 # 3) Debugear el contenido de una variable
 # 4) Saltos de linea o tabulador en un String
-# 5) 
-# 6) 
-# 7) 
+# 5) Function con paramaetros opcionales
+# 6) La mejor Practica con funciones es el return
+# 7) Variables locales y globales
 # 8) 
 # 9) 
 # 10) 
@@ -40,21 +40,48 @@ var_dump($variable);
 #----------------------------------------------------------------------------------------------------------------------------------------------
 # 4) Saltos de linea o tabulador en un String
 
-$text = "Soy un texto y \n HOla \t Salto de tabulador"
+$text = "Soy un texto y \n HOla \t Salto de tabulador";
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 5) 
+# 5) Function con paramaetros opcionales
+function sumar($numero1, $numero2, $numero3 = false){ 
+    if ( isset($numero3) ) { 
+      echo "<h1>" . ($numero1 + $numero2) . "</h1>";
+    }else{
+      echo "<h1>" . ($numero1 + $numero2 + $numero3) . "</h1>";
+    }
+}
 
+sumar(2, 3);
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 6) 
+# 6) La mejor Practica con funciones es el return
+function sumar2($numero1, $numero2, $numero3 = false){ 
+    if ( isset($numero3) ) { 
+        $resul = "<h1>" . ($numero1 + $numero2) . "</h1>";
+    }else{
+        $resul = "<h1>" . ($numero1 + $numero2 + $numero3) . "</h1>";
+    }
+    return $resul;
+}
 
+sumar(2, 3);
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 7) 
+# 7) Variables locales y globales
+$frase = "<h1>Hola soy Luis</h1>";
 
+echo $frase;
+
+function hola(){ 
+  # Para usar la variable de fuera se usa global
+  global $frase; 
+    return $frase;
+ }
+
+ hola();
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
