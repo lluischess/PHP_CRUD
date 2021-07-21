e6cf85d50d12234737f59cc0cc7197d6dc469ecd
Lluis Casamajor <lluischess@gmail.com>
2021-07-21 22:03:26 +0200

    arrays

--- Tips.php
+++ Tips.php
@@ -13,6 +13,9 @@
 # 10) Funciones Matematicas
 # 11) Mas funciones Predefinidas
 # 12) Include y require
+# 13) Arrays
+# 14) Arrays Multidimensionales
+# 15) Funciones para Arrays
 
 
 
@@ -174,18 +177,113 @@ require '<EjerciciosBasicos>/Ejercicio1.php'; // es necesario para continuar
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 12) 
+# 13) Arrays
 
+$pelicula = "spiderman";
+
+# Definir un array 2 formas:
+$peliculas = array('batman',$pelicula,'batman2');
+$cantantes = ['Justin','selena','lady gaga'];
+
+#Array asociativo
+$personas = array(
+  'nombre' => 'luis',
+  'apellido' => 'casamajor',
+  'edad'  => '27'
+);
+
+#array completa
+var_dump($peliculas);
+var_dump($cantantes);
+
+# sacar valores de un array
+var_dump($peliculas[1]);
+echo $peliculas[0];
+
+# Añadir elementos a la Array 2 maneras
+$peliculas[] = 'pokemon';
+array_push($peliculas,'digimon');
+var_dump($peliculas);
+
+#Eliminar el ultimo registro
+array_pop($peliculas);
+var_dump($peliculas);
+
+#Eliminar el indice que quieras
+unset($peliculas[2]);
+var_dump($peliculas);
+
+# Recorrer un array sencilla 2 maneras:
+for ($i=0; $i < count($peliculas); $i++) { 
+  echo "<li>" . $peliculas[$i] . "</li>";
+}
+
+foreach ($cantantes as $cantante) {
+  echo "<li>" . $cantante . "</li>";
+}
+
+foreach ($personas as $persona) {
+  echo "<li>" . $persona . "</li>";
+}
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 12) 
+# 14) Arrays Multidimensionales
 
+$contactos = array(
+  array(
+    'nombre' => 'luis',
+    'email' => 'casamajor@email.com',
+  ),
+  array(
+    'nombre' => 'loco',
+    'email' => 'casamajor@email.com',
+  ),
+  array(
+    'nombre' => 'luis',
+    'email' => 'casamajor@email.com',
+  )
+);
 
+var_dump($contactos);
+
+# imprimir un json de una array multidimensional
+echo json_encode($contactos);
+# acceder a un contacto de un array multidemensional
+echo $contactos[1]['nombre'];
+
+# Recorrer un array multidimensional:
+foreach ($contactos as $key => $contacto){ 
+  echo "<br>" . $contacto['nombre'];
+  echo "<br>" . $contacto['email'];
+
+}
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 12) 
+# 15) Funciones para Arrays
+$cantantes = ['Justin','selena','lady gaga'];
+$num = [4,3,2,1];
 
+# Orden alfabetico A - Z
+asort($cantantes);
+var_dump($cantantes);
+
+# Orden alfabetico inverso Z - A
+arsort($cantantes);
+var_dump($cantantes);
+
+# Orden numerico 1 - max
+sort($num);
+var_dump($num);
+
+# Sacr un elemento aleatorio de un array
+echo array_rand($cantantes);
+
+# Revertir el Array dar la vuelta
+var_dump(array_reverse($peliculas));
+
+# Buscar dentro de un array
+echo array_search('selena',$cantantes);
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
