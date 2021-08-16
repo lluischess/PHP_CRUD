a2cbefb5bea2b4d51e9ca68ce726efd3bf987081
Lluis Casamajor <lluischess@gmail.com>
2021-08-16 14:26:57 +0200

    bbddandphp

--- PHPyMYSQL/tipsphpmysql.php
+++ PHPyMYSQL/tipsphpmysql.php
@@ -1,9 +1,9 @@
 <?php
 
 # INDEX 
-# 1) Conectar a MYsql desde php
-# 2) 
-# 3) 
+# 1) Conectar a MYsql desde php clasica
+# 2) Consulta para configurar la codificación de caracteres
+# 3) Consulta para mostrar datos
 # 4) 
 # 5) 
 # 6) 
@@ -14,8 +14,31 @@
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 1) Conectar a MYsql desde php
+# 1) Conectar a MYsql desde php clasica
+
+$conecxion = mysqli_connect("localhost","root","","phpmysql");
+
+# comprobar conexion:
+
+if(mysqli_connect_errno($conecxion)) {
+    echo "la conexion a fallado" . mysqli_connect_errno($conecxion);
+}else{
+    echo "conexion realizada correctamente";
+}
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 2)
+# 2) Consulta para configurar la codificación de caracteres
+
+mysqli_query($conecxion,"SET NAMES ''Utf8");
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 3) Consulta para mostrar datos
+
+$notas = mysqli_query($conecxion,"SELECT * FROM notas");
+
+#$notas = mysqli_fetch_assoc($notas);
+
+while($nota = mysqli_fetch_assoc($notas)) {
+    var_dump($nota);
+}
