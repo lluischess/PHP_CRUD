433131b6a42d64a46e6680f5ec924c72ecffcf4b
Lluis Casamajor <lluischess@gmail.com>
2021-08-17 08:52:27 +0200

    insert sql

--- PHPyMYSQL/tipsphpmysql.php
+++ PHPyMYSQL/tipsphpmysql.php
@@ -3,8 +3,8 @@
 # INDEX 
 # 1) Conectar a MYsql desde php clasica
 # 2) Consulta para configurar la codificación de caracteres
-# 3) Consulta para mostrar datos
-# 4) 
+# 3) Mostrar datos
+# 4) Insertar datos
 # 5) 
 # 6) 
 # 7) 
@@ -33,7 +33,7 @@ if(mysqli_connect_errno($conecxion)) {
 mysqli_query($conecxion,"SET NAMES ''Utf8");
 
 #----------------------------------------------------------------------------------------------------------------------------------------------
-# 3) Consulta para mostrar datos
+# 3) Mostrar datos
 
 $notas = mysqli_query($conecxion,"SELECT * FROM notas");
 
@@ -41,4 +41,34 @@ $notas = mysqli_query($conecxion,"SELECT * FROM notas");
 
 while($nota = mysqli_fetch_assoc($notas)) {
     var_dump($nota);
+    echo $nota['Titulo'] . "<br>";
+    echo $nota['Descripcion']. "<br>";
+    echo $nota['Color']. "<br>";
+
 }
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 4) Insertar datos
+
+$insert_sql = "INSERT INTO notas(Titulo,Descripcion,Color) VALUES('Nota desde PHP','descripcion de la nota php', 'naranja')";
+
+$insert = mysqli_query($conecxion,$insert_sql);
+
+if($insert){
+    echo "Datos insertados correctamente";
+}else{
+    echo "Error: " . mysqli_error($conecxion);
+}
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
